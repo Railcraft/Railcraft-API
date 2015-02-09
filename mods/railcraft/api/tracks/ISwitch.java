afb7dc18eabeedd7de1cfeaa84fd9ad425c523a7
Sarit Sotangkur <ssotangkur@users.noreply.github.com>
2015-02-08 23:27:43 -0800

    Modified API to enable better switching

---
+++ mods/railcraft/api/tracks/ISwitch.java
@@ -0,0 +1,20 @@
+package mods.railcraft.api.tracks;
+
+import net.minecraft.entity.item.EntityMinecart;
+
+public interface ISwitch {
+
+    /**
+     * This method is used by the <code>switchTrack</code> to ask the switch
+     * device whether it thinks the track should be switched or not. Ultimately,
+     * the track itself will decide whether it will be switched, however the
+     * track will usually try to honor results of this method when possible.
+     * 
+     * @param switchTrack The switch track that is asking
+     * @param cart The cart that the switch may use to determine switch status.
+     * Implementations should expect null values.
+     * @return true if the switch would like the track switched
+     */
+    public boolean shouldSwitch(ITrackSwitch switchTrack, EntityMinecart cart);
+
+}
