5481c0368d01ed2851c5d81d9900e1dc7af8e512
CovertJaguar <covertjaguar@gmail.com>
2016-02-28 12:03:42 -0800

    Update ITrackItem interface

--- mods/railcraft/api/core/items/ITrackItem.java
+++ mods/railcraft/api/core/items/ITrackItem.java
@@ -11,46 +11,44 @@ package mods.railcraft.api.core.items;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 /**
  * Should be implemented by any rail item class that wishes to have
  * it's rails placed by for example the Tunnel Bore or Track Relayer.
- *
+ * <p/>
  * If you defined your rails with a TrackSpec, you don't need to worry about this.
  *
  * @author CovertJaguar <http://www.railcraft.info>
  */
-public interface ITrackItem
-{
+public interface ITrackItem {
 
     /**
      * Attempts to place a track.
      *
      * @param stack
      * @param world The World object
-     * @param i x-Coord
-     * @param j y-Coord
-     * @param k z-Coord
+     * @param pos   the position
      * @return true if successful
      */
-    public boolean placeTrack(ItemStack stack, World world, int i, int j, int k);
+    boolean placeTrack(ItemStack stack, World world, BlockPos pos);
 
     /**
      * Return the block of a placed track.
      *
      * @return the blockId
      */
-    public Block getPlacedBlock();
+    Block getPlacedBlock();
 
     /**
      * Return true if the given tile entity corresponds to this Track item.
-     *
+     * <p/>
      * If the track has no tile entity, return true on null.
      *
      * @param stack
      * @param tile
      * @return
      */
-    public boolean isPlacedTileEntity(ItemStack stack, TileEntity tile);
+    boolean isPlacedTileEntity(ItemStack stack, TileEntity tile);
 }
