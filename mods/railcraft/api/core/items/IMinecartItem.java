7c632a901644ca79121bcc8ac5654393f9cd7175
CovertJaguar <covertjaguar@gmail.com>
2016-02-28 13:24:37 -0800

    Fix IMinecraftItem

--- mods/railcraft/api/core/items/IMinecartItem.java
+++ mods/railcraft/api/core/items/IMinecartItem.java
@@ -11,6 +11,7 @@ package mods.railcraft.api.core.items;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 /**
@@ -19,31 +20,26 @@ import net.minecraft.world.World;
  *
  * @author CovertJaguar <http://www.railcraft.info>
  */
-public interface IMinecartItem
-{
+public interface IMinecartItem {
 
     /**
      * Controls whether this cart item can be placed by the Cart and Train Dispensers.
-     *
+     * <p/>
      * Generally, you can ignore the placeCart() function if this returns false.
      *
-     * @param cart
      * @return true if it can be placed, false otherwise
      */
-    public boolean canBePlacedByNonPlayer(ItemStack cart);
+    boolean canBePlacedByNonPlayer(ItemStack cart);
 
     /**
      * Places a cart at the specified location.
-     *
+     * <p/>
      * Implementing this function is optional.
      *
      * @param owner the name of the player placing the cart or "[MyMod]" with the brackets
-     * @param cart An ItemStack that contains the cart
+     * @param cart  An ItemStack that contains the cart
      * @param world The World
-     * @param i x-Coord
-     * @param j y-Coord
-     * @param k z-Coord
      * @return the cart placed or null if failed
      */
-    public EntityMinecart placeCart(GameProfile owner, ItemStack cart, World world, int i, int j, int k);
+    EntityMinecart placeCart(GameProfile owner, ItemStack cart, World world, BlockPos pos);
 }
