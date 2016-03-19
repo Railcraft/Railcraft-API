1066b8d5612a989cc29b9aa873cac1dd427cc761
CovertJaguar <covertjaguar@gmail.com>
2016-03-19 03:19:42 -0700

    Store the signal debug boolean in the API.

--- mods/railcraft/api/signals/package-info.java
+++ mods/railcraft/api/signals/package-info.java
@@ -5,6 +5,6 @@
  *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
  * ***************************************************************************
  */
-@API(apiVersion="2.0.1", owner="RailcraftAPI|core", provides="RailcraftAPI|signals")
+@API(apiVersion="2.1.0", owner="RailcraftAPI|core", provides="RailcraftAPI|signals")
 package mods.railcraft.api.signals;
 import cpw.mods.fml.common.API;
