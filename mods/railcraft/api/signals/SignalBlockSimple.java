96cfe5f26ddafb99514d82c5e9af88dd2f03f289
CovertJaguar <covertjaguar@gmail.com>
2016-03-19 17:13:15 -0700

    Update file headers

--- mods/railcraft/api/signals/SignalBlockSimple.java
+++ mods/railcraft/api/signals/SignalBlockSimple.java
@@ -4,6 +4,7 @@
  * This work (the API) is licensed under the "MIT" License,
  * see LICENSE.md for details.
  ******************************************************************************/
+
 package mods.railcraft.api.signals;
 
 import mods.railcraft.api.core.WorldCoordinate;
--- mods/railcraft/api/signals/SimpleSignalController.java
+++ mods/railcraft/api/signals/SimpleSignalController.java
@@ -1,10 +1,9 @@
-/*
- * ******************************************************************************
- *  Copyright 2011-2015 CovertJaguar
+/*******************************************************************************
+ * Copyright (c) CovertJaguar, 2011-2016
  *
- *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
- * ***************************************************************************
- */
+ * This work (the API) is licensed under the "MIT" License,
+ * see LICENSE.md for details.
+ ******************************************************************************/
 package mods.railcraft.api.signals;
 
 import mods.railcraft.api.core.WorldCoordinate;
