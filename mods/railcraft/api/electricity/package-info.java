fe81aefd03c297ff3aaa2461d988770ebf6227b3
CovertJaguar <covertjaguar@gmail.com>
2016-05-07 22:53:07 -0700

    Bump the version for the electricity package.

--- mods/railcraft/api/electricity/package-info.java
+++ mods/railcraft/api/electricity/package-info.java
@@ -5,6 +5,6 @@
  *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
  * ***************************************************************************
  */
-@API(apiVersion="2.0.0", owner="RailcraftAPI|core", provides="RailcraftAPI|electricity")
+@API(apiVersion="3.0.0", owner="RailcraftAPI|core", provides="RailcraftAPI|electricity")
 package mods.railcraft.api.electricity;
 import net.minecraftforge.fml.common.API;
