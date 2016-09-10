cc223e0bd8e62fa6bf9af3f989b281ee96bd332a
CovertJaguar <covertjaguar@gmail.com>
2016-09-10 14:56:30 -0700

    Complete Fluid API migration

--- mods/railcraft/api/crafting/ICokeOvenCraftingManager.java
+++ mods/railcraft/api/crafting/ICokeOvenCraftingManager.java
@@ -1,16 +1,17 @@
-/*
- * ******************************************************************************
- *  Copyright 2011-2015 CovertJaguar
- *
- *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
- * ***************************************************************************
- */
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2016
+
+ This work (the API) is licensed under the "MIT" License,
+ see LICENSE.md for details.
+ -----------------------------------------------------------------------------*/
 
 package mods.railcraft.api.crafting;
 
-import java.util.List;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  *
@@ -20,6 +21,7 @@ public interface ICokeOvenCraftingManager {
 
     void addRecipe(ItemStack input, boolean matchDamage, boolean matchNBT, ItemStack output, FluidStack liquidOutput, int cookTime);
 
+    @Nullable
     ICokeOvenRecipe getRecipe(ItemStack stack);
 
     List<? extends ICokeOvenRecipe> getRecipes();
