3603bd0bf98e3cbd18da08046a370f37469823ec
CovertJaguar <covertjaguar@gmail.com>
2017-03-06 18:46:56 -0800

    Bump API version

--- mods/railcraft/api/core/items/package-info.java
+++ mods/railcraft/api/core/items/package-info.java
@@ -1,11 +1,10 @@
-/*
- * ******************************************************************************
- *  Copyright 2011-2015 CovertJaguar
- *
- *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
- * ***************************************************************************
- */
-@API(apiVersion = "2.0.0", owner = "RailcraftAPI|core", provides = "RailcraftAPI|items")
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2016
+
+ This work (the API) is licensed under the "MIT" License,
+ see LICENSE.md for details.
+ -----------------------------------------------------------------------------*/
+@API(apiVersion = "2.1.0", owner = "RailcraftAPI|core", provides = "RailcraftAPI|items")
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 package mods.railcraft.api.core.items;
