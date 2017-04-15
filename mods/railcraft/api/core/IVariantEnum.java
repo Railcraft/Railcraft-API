8bd8b75399f931078da97799863dc84639fa306d
CovertJaguar <covertjaguar@gmail.com>
2017-04-15 01:24:34 -0700

    Clean up Deprecation testing of Variants, removes old ores from creative

--- mods/railcraft/api/core/IVariantEnum.java
+++ mods/railcraft/api/core/IVariantEnum.java
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
- Copyright (c) CovertJaguar, 2011-2016
+ Copyright (c) CovertJaguar, 2011-2017
 
  This work (the API) is licensed under the "MIT" License,
  see LICENSE.md for details.
@@ -33,5 +33,9 @@ public interface IVariantEnum extends IStringSerializable {
 
     default boolean isEnabled() {
         return true;
+    }
+
+    default boolean isDeprecated() {
+        return false;
     }
 }
--- mods/railcraft/api/core/package-info.java
+++ mods/railcraft/api/core/package-info.java
@@ -4,7 +4,7 @@
  This work (the API) is licensed under the "MIT" License,
  see LICENSE.md for details.
  -----------------------------------------------------------------------------*/
-@API(apiVersion = "3.0.0", owner = RailcraftConstantsAPI.MOD_ID, provides = RailcraftConstantsAPI.API_ID + "core")
+@API(apiVersion = "3.1.0", owner = RailcraftConstantsAPI.MOD_ID, provides = RailcraftConstantsAPI.API_ID + "core")
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 package mods.railcraft.api.core;
