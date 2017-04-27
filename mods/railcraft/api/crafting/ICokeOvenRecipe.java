15870c6efda20f0a6f2d8ffc2ac69d6898bb18a7
CovertJaguar <covertjaguar@gmail.com>
2017-04-27 15:33:32 -0700

    More prep for 1.11

--- mods/railcraft/api/crafting/ICokeOvenRecipe.java
+++ mods/railcraft/api/crafting/ICokeOvenRecipe.java
@@ -1,28 +1,29 @@
-/*
- * ******************************************************************************
- *  Copyright 2011-2015 CovertJaguar
- *
- *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
- * ***************************************************************************
- */
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2017
+
+ This work (the API) is licensed under the "MIT" License,
+ see LICENSE.md for details.
+ -----------------------------------------------------------------------------*/
 
 package mods.railcraft.api.crafting;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
+import javax.annotation.Nullable;
+
 /**
- *
  * @author CovertJaguar <http://www.railcraft.info>
  */
-public interface ICokeOvenRecipe
-{
+public interface ICokeOvenRecipe {
 
     int getCookTime();
 
     ItemStack getInput();
 
+    @Nullable
     FluidStack getFluidOutput();
 
+    @Nullable
     ItemStack getOutput();
 }
