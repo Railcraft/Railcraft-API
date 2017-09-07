d0a6a0e1d2f036e22fc76ac116a074d10c484187
CovertJaguar <covertjaguar@gmail.com>
2017-09-07 02:19:25 -0700

    Restore Metal Posts and Platforms

--- mods/railcraft/api/core/IPostConnection.java
+++ mods/railcraft/api/core/IPostConnection.java
@@ -1,17 +1,19 @@
-/*
- * ******************************************************************************
- *  Copyright 2011-2015 CovertJaguar
- *
- *  This work (the API) is licensed under the "MIT" License, see LICENSE.md for details.
- * ***************************************************************************
- */
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2017
+
+ This work (the API) is licensed under the "MIT" License,
+ see LICENSE.md for details.
+ -----------------------------------------------------------------------------*/
 
 package mods.railcraft.api.core;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+
+import java.util.Locale;
 
 /**
  * If you want your block to connect (or not connect) to posts, implement this
@@ -23,11 +25,16 @@ import net.minecraft.world.IBlockAccess;
  */
 public interface IPostConnection {
 
-    enum ConnectStyle {
+    enum ConnectStyle implements IStringSerializable {
 
         NONE,
         SINGLE_THICK,
-        TWO_THIN,
+        TWO_THIN;
+
+        @Override
+        public String getName() {
+            return name().toLowerCase(Locale.ROOT);
+        }
     }
 
     /**
@@ -35,7 +42,7 @@ public interface IPostConnection {
      * connects to a post.
      *
      * @param world The World
-     * @param pos Our position
+     * @param pos   Our position
      * @param state Our BlockState
      * @param side  Side to connect to
      * @return true if connect
