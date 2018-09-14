c0d1f25ed650dc874350f038fd613f509c825533
liach <liach@users.noreply.github.com>
2018-09-14 05:02:56 -0700

    Document more clearly on the "reverse" feature of a reversible track kit
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/mods/railcraft/api/tracks/ITrackKitReversible.java
+++ src/main/java/mods/railcraft/api/tracks/ITrackKitReversible.java
@@ -10,14 +10,29 @@ package mods.railcraft.api.tracks;
 /**
  * Implementing this interface will allow your track to be direction specific.
  *
- * And so long as you inherit from TrackInstanceBase it will automatically be
- * reversible via the Crowbar.
+ * <p>So long as you inherit from TrackInstanceBase it will automatically be
+ * reversible via the Crowbar.</p>
+ *
+ * <p>When the track kit is facing {@link net.minecraft.util.EnumFacing#SOUTH south} or
+ * {@link net.minecraft.util.EnumFacing#WEST west}, it is considered reversed.</p>
  *
  * @author CovertJaguar <http://www.railcraft.info>
  */
 public interface ITrackKitReversible extends ITrackKitInstance {
 
+    /**
+     * Returns true if this track kit instance is currently reversed, or currently facing
+     * south or west.
+     *
+     * @return True if the track kit is reversed
+     */
     boolean isReversed();
 
+    /**
+     * Set the track kit to be reversed, or be facing south or west (dependent on the rail
+     * direction).
+     *
+     * @param reversed Whether the track kit is reversed
+     */
     void setReversed(boolean reversed);
 }
